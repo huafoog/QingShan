00981e7c956c37d28de5c0df66d9d60b965e957f
青杉 <zhangyaxing521@qq.com>
2021-04-12 09:11:48 +0800

    1

--- QingShan.Web/Startup.cs
+++ QingShan.Web/Startup.cs
@@ -61,6 +61,7 @@ namespace QingShan.Core.Web
             });
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
